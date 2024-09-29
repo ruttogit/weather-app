f28b075407dc7e54a9c1b6ca3515f8659846d8d9
Dennis Kipkoech <kipkoechd09@gmail.com>
2024-09-29 10:26:27 +0300

    added loaders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import { BarLoader } from "react-spinners";
 import Header from "./components/Header/Header";
 import WeatherDetails from "./components/WeatherDetails/WeatherDetails";
 import useFetch from "./hooks/useFetch";
 import { useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+import { Bars } from "react-loader-spinner";
 
 function App() {
 
   const [searchQuery, setSearchQuery] = useState('Eldoret');
   const {error, loading, weatherData} = useFetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&appid=e65158f1c8f2a433ae768b6ca1002967&units=imperial`)
-  if (loading) return <p>Loading</p>
+  if (loading) return <div className="spiner">
+    <Bars
+  height="80"
+  width="80"
+  color="#4fa94d"
+  ariaLabel="bars-loading"
+  wrapperStyle={{}}
+  wrapperClass=""
+  visible={true}
+  />
+  </div> 
   if (error) return <p>Error!</p>
   if (weatherData.cod === '404') return <h1>The city Was no found Retry Again</h1>
 
